a9bf5377b9ac06c72780506521be6cec0d5e8f59
Louise Skinnari <louise.skinnari@cern.ch>
2016-05-09 09:53:12 -0500

    adding crab configs

---
+++ crabConfig_WJets_HT-100To200.py
@@ -0,0 +1,27 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'WJets_HT-100To200'
+config.General.workArea = 'test'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.inputFiles = ['FrameworkJobReport.xml', 'execute_for_crab.py', 'iheartny_topxs_fwlite.py', 'JECs', 'pileup_reweight.root']
+config.JobType.outputFiles = ['test_iheartNY.root']
+config.JobType.scriptExe = 'execute_iheartNY_MC.sh'
+
+config.section_("Data")
+config.Data.inputDataset = '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/jkarancs-B2GAnaFW_76X_V1p1_RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1-bf3ef703e3bdb5dcc5320cf3ff6ce74d/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+#config.Data.totalUnits = 1000
+config.Data.ignoreLocality = True
+config.Data.publication = False
+config.Data.outLFNDirBase = '/store/user/skinnari/TopXS_13TeV'
+
+config.section_("Site")
+config.Site.storageSite = 'T3_US_FNALLPC'
