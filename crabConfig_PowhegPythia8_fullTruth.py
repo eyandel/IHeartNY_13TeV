4a231d564a87dc034d20cd651d1f52bcd1829a9b
Susan Dittmer <silvergrey.sjd@gmail.com>
2016-07-29 15:07:39 +0200

    Edits for fullTruth

---
+++ crabConfig_PowhegPythia8_fullTruth.py
@@ -0,0 +1,29 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'PowhegPythia8_fullTruth'
+config.General.workArea = 'test'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.inputFiles = ['FrameworkJobReport.xml', 'execute_for_crab.py', 'iheartny_topxs_fwlite.py', 'JECs', 'pileup_reweight.root']
+config.JobType.outputFiles = ['test_iheartNY.root']
+config.JobType.scriptExe = 'execute_iheartNY_ttbar_fullTruth.sh'
+
+config.section_("Data")
+config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/vorobiev-B2GAnaFW_RunIIFall15MiniAODv2_25ns_v76x_v1p2-bf3ef703e3bdb5dcc5320cf3ff6ce74d/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+#config.Data.totalUnits = 1000
+config.Data.ignoreLocality = True
+config.Data.publication = False
+config.Data.outLFNDirBase = '/store/user/dittmer/13TeV'
+# This string is used to construct the output dataset name
+
+config.section_("Site")
+config.Site.storageSite = 'T3_US_FNALLPC'
+#config.Site.whitelist = ["T2_HU_Budapest"]
